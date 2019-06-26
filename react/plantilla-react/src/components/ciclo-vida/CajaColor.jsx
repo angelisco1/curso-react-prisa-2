196e6cad155a803d836cd44abd3bf9fc1bb20f06
AngelVil92 <angelvil92@gmail.com>
2019-06-26 08:47:48 +0200

    Esqueleto ejemplos ciclo de vida

---
+++ react/plantilla-react/src/components/ciclo-vida/CajaColor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CajaColor extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default CajaColor;
---
+++ react/plantilla-react/src/components/ciclo-vida/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import CajaColor from './CajaColor';
+import CuentaAtras from './CuentaAtras';
+
+class CicloVidaCmp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 1
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ciclo de vida</h1>
+      </div>
+    )
+  }
+}
+
+export default CicloVidaCmp;
