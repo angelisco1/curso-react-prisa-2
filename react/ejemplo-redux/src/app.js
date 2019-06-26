b51b8fb652c9960964a0f3cd88d4ba84b43a5cda
AngelVil92 <angelvil92@gmail.com>
2019-06-26 17:51:44 +0200

    Plantilla proyecto Redux

---
+++ react/ejemplo-redux/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Redux</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
